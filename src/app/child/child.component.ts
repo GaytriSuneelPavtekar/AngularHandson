332ba5f1c4bcdb06461c73413183eb3e9acada5f
Gaytri Suneel Pavtekar <pavtekargaytri@gmail.com>
2021-08-24 18:51:53 +0530

    Updating child component

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+@Input() public parentData : string = "";
+@Output() public childEvent = new EventEmitter
+public fun(){
+  this.childEvent.emit("hellof rom child");
+  console.log('Clicked');
+}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
