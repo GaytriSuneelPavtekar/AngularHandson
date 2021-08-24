4fa4b35730b7cab684531618e5505367d9aa17d1
Gaytri Suneel Pavtekar <pavtekargaytri@gmail.com>
2021-08-24 22:25:07 +0530

    Updated App component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomDirectiveDemoDirective } from './custom-directive-demo.directive';
+import { BootstrapdemoComponent } from './bootstrapdemo/bootstrapdemo.component';
+import {AlertModule } from 'ngx-bootstrap/alert';
+import { ChildComponent } from './child/child.component';
+import { TwoWaybindingComponent } from './two-waybinding/two-waybinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomDirectiveDemoDirective
+    CustomDirectiveDemoDirective,
+    BootstrapdemoComponent,
+    ChildComponent,
+    TwoWaybindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
