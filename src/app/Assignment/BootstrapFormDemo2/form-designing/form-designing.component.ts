d3a0307a4cb51001e096ea53d5250a3c431b2a21
Gaytri Suneel Pavtekar <pavtekargaytri@gmail.com>
2021-09-08 15:40:56 +0530

    Added Angular Form Designing

---
+++ src/app/Assignment/BootstrapFormDemo2/form-designing/form-designing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-designing',
+  templateUrl: './form-designing.component.html',
+  styleUrls: ['./form-designing.component.css']
+})
+export class FormDesigningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
