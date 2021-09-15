c10db4cef92834b6811dbd8c8b4ac88ec14b1b67
Gaytri Suneel Pavtekar <pavtekargaytri@gmail.com>
2021-09-15 17:57:33 +0530

    Added material design demonstration

---
+++ src/app/LectureHandson/material-design-demo/material-design-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-material-design-demo',
+  templateUrl: './material-design-demo.component.html',
+  styleUrls: ['./material-design-demo.component.css']
+})
+export class MaterialDesignDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
